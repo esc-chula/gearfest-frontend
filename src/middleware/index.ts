e48e6cd3141b63675a28ef125349e72dff299717
shalluv <leon555thai@gmail.com>
2023-12-25 20:38:39 +0700

    style: format middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,8 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-	// TODO: Implement auth routes logic
-	console.log('Middleware works!')
+  // TODO: Implement auth routes logic
+  console.log("Middleware works!");
 
-	return next();
+  return next();
 });
