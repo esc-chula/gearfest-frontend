a10037b4a31e6a508d23b94bdbc4c0f55da8994e
mildrrnt <woranita888@gmail.com>
2024-01-04 00:54:53 +0700

    feat: added elements

---
+++ src/components/story/FooterDecorator.tsx
@@ -0,0 +1,18 @@
+const FooterDecorator = () => {
+    return(
+        <div className="opacity-80 absolute inset-x-0 bottom-[8rem] -left-[1.25rem]">
+            <img className="w-[152px] h-32 left-0 top-[48px] absolute" src="/images/elements/calendar.png" />
+            <img className="w-[140.50px] h-[166.91px] left-[471.56px] top-[8.91px] absolute origin-top-left rotate-[62.88deg]" src="/images/elements/pallette.png" />
+            <img className="w-24 h-[90px] left-[193px] top-0 absolute" src="/images/elements/heart.png" />
+            <img className="w-[109.92px] h-[105.82px] left-[226.20px] top-[15px] absolute origin-top-left rotate-[1.19deg]" src="/images/elements/citrus.png" />
+            <img className="w-[118px] h-[104px] left-[142px] top-[45px] absolute" src="/images/elements/donut.png" />
+            <img className="w-[70px] h-[121px] left-[117px] top-[45px] absolute" src="/images/elements/smoothie.png" />
+            <img className="w-[129.02px] h-[115.51px] left-[226px] top-[67.43px] absolute origin-top-left rotate-[-17.79deg]" src="/images/elements/letter.png" />
+            <img className="w-[88px] h-[88px] left-[207px] top-[69px] absolute" src="/images/elements/strawberry.png" />
+            <img className="w-[84px] h-[82px] left-[315px] top-[71px] absolute" src="/images/elements/cake.png" />
+        </div>
+    )
+
+}
+
+export default FooterDecorator;
