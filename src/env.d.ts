6dc6ecbd2d5bbf076b160b9b2e74b06088a83fa3
paepaev2 <p.mwtr99@gmail.com>
2024-01-08 13:30:55 +0700

    feat: add middleware

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,3 +7,14 @@ interface ImportMetaEnv {
   interface ImportMeta {
     readonly env: ImportMetaEnv
   }
+
+  /// <reference types="astro/client" />
+declare namespace App {
+  interface Locals {
+      user: {
+          name: string
+      },
+      welcomeTitle: () => string,
+      orders: Map<string, object>
+  }
+}
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,17 @@
 import { defineMiddleware } from "astro:middleware";
+import { sequence } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
   // TODO: Implement auth routes logic
   console.log("Middleware works!");
+  auth(??);
 
   return next();
 });
+
+async function auth(_, next) {
+  console.log("auth request");
+  const response = await next();
+  console.log("auth response");
+  return response;
+}
