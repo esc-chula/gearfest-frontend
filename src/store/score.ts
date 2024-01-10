048d64304d9b2702a5b2d46207f245a8c4c7cc81
shalluv <leon555thai@gmail.com>
2024-01-10 12:55:04 +0700

    chore(type): add typing in score store

--- src/store/score.ts
+++ src/store/score.ts
@@ -1,18 +1,17 @@
 import { persistentAtom } from "@nanostores/persistent";
 
-const initialValue = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
+const initialValue: Record<number, number> = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
 
 const scores = persistentAtom("scores", initialValue, {
   encode: JSON.stringify,
   decode: JSON.parse,
 });
 
-export function addScores(values) {
-  console.log(scores.get());
+export function addScores(values: Record<number, number>) {
   const updatedScores = scores.get();
-  Object.keys(updatedScores).forEach((key, index) => {
-    updatedScores[key] += values[index];
-  });
+  for (const key in values) {
+    updatedScores[key] += values[key];
+  }
   scores.set(updatedScores);
 }
 
