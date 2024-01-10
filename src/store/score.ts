50c269e2ae94fd7c35030d116076ae2fc555b62a
Worranittha H <86171659+mildrrnt@users.noreply.github.com>
2024-01-10 13:04:50 +0700

    Add scoring system (styled) (#4)
    * feat: update scores
    
    * chore: removed comments
    
    * style: apply Prettier
    
    * chore(type): add typing in score store
    
    * chore: remove temporary testing files
    
    * chore(style): prettier score.ts
    
    ---------
    
    Co-authored-by: shalluv <leon555thai@gmail.com>

--- src/store/score.ts
+++ src/store/score.ts
@@ -1,1 +1,25 @@
-// TODO: Implement score store if needed
+import { persistentAtom } from "@nanostores/persistent";
+
+const initialValue: Record<number, number> = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+};
+
+const scores = persistentAtom("scores", initialValue, {
+  encode: JSON.stringify,
+  decode: JSON.parse,
+});
+
+export function addScores(values: Record<number, number>) {
+  const updatedScores = scores.get();
+  for (const key in values) {
+    updatedScores[key] += values[key];
+  }
+  scores.set(updatedScores);
+}
+
+export default scores;
