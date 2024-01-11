b2afcd4e4bf2d4bae7f94c2cbe2489c05a48af3f
mildrrnt <woranita888@gmail.com>
2024-01-12 00:13:59 +0700

    refactor: move images to /src

--- src/components/story/FooterDecorator.tsx
+++ src/components/story/FooterDecorator.tsx
@@ -3,39 +3,39 @@ const FooterDecorator = () => {
     <div className="absolute inset-x-0 -left-[1.25rem] bottom-[8rem] opacity-80">
       <img
         className="absolute left-0 top-[48px] h-32 w-[152px]"
-        src="/images/elements/calendar.png"
+        src="/src/images/elements/calendar.png"
       />
       <img
         className="absolute left-[471.56px] top-[8.91px] h-[166.91px] w-[140.50px] origin-top-left rotate-[62.88deg]"
-        src="/images/elements/pallette.png"
+        src="/src/images/elements/pallette.png"
       />
       <img
         className="absolute left-[193px] top-0 h-[90px] w-24"
-        src="/images/elements/heart.png"
+        src="/src/images/elements/heart.png"
       />
       <img
         className="absolute left-[226.20px] top-[15px] h-[105.82px] w-[109.92px] origin-top-left rotate-[1.19deg]"
-        src="/images/elements/citrus.png"
+        src="/src/images/elements/citrus.png"
       />
       <img
         className="absolute left-[142px] top-[45px] h-[104px] w-[118px]"
-        src="/images/elements/donut.png"
+        src="/src/images/elements/donut.png"
       />
       <img
         className="absolute left-[117px] top-[45px] h-[121px] w-[70px]"
-        src="/images/elements/smoothie.png"
+        src="/src/images/elements/smoothie.png"
       />
       <img
         className="absolute left-[226px] top-[67.43px] h-[115.51px] w-[129.02px] origin-top-left rotate-[-17.79deg]"
-        src="/images/elements/letter.png"
+        src="/src/images/elements/letter.png"
       />
       <img
         className="absolute left-[207px] top-[69px] h-[88px] w-[88px]"
-        src="/images/elements/strawberry.png"
+        src="/src/images/elements/strawberry.png"
       />
       <img
         className="absolute left-[315px] top-[71px] h-[82px] w-[84px]"
-        src="/images/elements/cake.png"
+        src="/src/images/elements/cake.png"
       />
     </div>
   );
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,26 @@
-const { addDynamicIconSelectors } = require("@iconify/tailwind");
+import { addDynamicIconSelectors } from "@iconify/tailwind";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      body: [],
-    },
     extend: {
+      fontFamily: {
+        sans: ["SukhumvitSet", ...defaultTheme.fontFamily.sans],
+      },
       backgroundImage: {
-        raspberry: 'url("/images/elements/grain-bg.png"), linear-gradient(170deg, #F9998C -10.14%, #650C17 98.39%)',
+        raspberry: 'url("/src/images/elements/grain-bg.png"), linear-gradient(170deg, #F9998C -10.14%, #650C17 98.39%)',
         raspberrybox: 'linear-gradient(166deg, #FFFDF9 -16.07%, #ECC7BB 81.36%)',
-        cosmo: 'url("/images/elements/grain-bg.png"), linear-gradient(170deg, #F9A68B -10.14%, #C04343 98.39%)',
+        cosmo: 'url("/src/images/elements/grain-bg.png"), linear-gradient(170deg, #F9A68B -10.14%, #C04343 98.39%)',
         cosmobox: 'linear-gradient(166deg, #FFFFFF -16.07%, #ECC8BB 81.36%)',
-        pina: 'url("/images/elements/grain-bg.png"), linear-gradient(170deg, #F9C08C -10.14%, #CB6100 98.39%)',
+        pina: 'url("/src/images/elements/grain-bg.png"), linear-gradient(170deg, #F9C08C -10.14%, #CB6100 98.39%)',
         pinabox: 'linear-gradient(166deg, #FFFDF9 -16.07%, #ECD3BB 81.36%)',
-        gin: 'url("/images/elements/grain-bg.png"), linear-gradient(170deg, #B2C196 -10.14%, #0D5649 98.39%)',
+        gin: 'url("/src/images/elements/grain-bg.png"), linear-gradient(170deg, #B2C196 -10.14%, #0D5649 98.39%)',
         ginbox: 'linear-gradient(166deg, #FFFFFF -16.07%, #C7DBB7 81.36%)',
-        lagoon: 'url("/images/elements/grain-bg.png"), linear-gradient(170deg, #84C9EF -10.14%, #0D3C56 98.39%)',
+        lagoon: 'url("/src/images/elements/grain-bg.png"), linear-gradient(170deg, #84C9EF -10.14%, #0D3C56 98.39%)',
         lagoonbox: 'linear-gradient(166deg, #FAFEFF -16.07%, #B1DCFC 81.36%)',
-        lalemon: 'url("/images/elements/grain-bg.png"), linear-gradient(170deg, #D09DDE -10.14%, #30255A 98.39%)',
+        lalemon: 'url("/src/images/elements/grain-bg.png"), linear-gradient(170deg, #D09DDE -10.14%, #30255A 98.39%)',
         lalemonbox: 'linear-gradient(166deg, #FBF6FE -16.07%, #E2C9E8 81.36%)',
 
       },
