74b4c1ee3cb23ae5d6581dd0162c28c8029412a9
kikinnicha <kinatnicha@gmail.com>
2024-01-15 20:01:43 +0700

    chore: Hero component layout

---
+++ src/components/register-page/Hero.tsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <div className="w-full space-y-12">
+      <div className="flex h-72 w-full items-end overflow-hidden rounded-lg bg-white">
+        {/* <Image src={BarImage} alt="Bar Image" /> */}
+      </div>
+      <div>
+        <button className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-base px-4 py-2.5 text-center text-2xl font-semibold text-primary shadow-button">
+          <i className="icon-[devicon--google] h-7 w-7"></i>
+          Register with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
