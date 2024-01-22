cac4cd6d313c5cca3104f9280a22867e6ca48275
shalluv <leon555thai@gmail.com>
2024-01-22 18:58:49 +0700

    refactor(cards): refactor codes

--- src/components/story/ButtonSet.tsx
+++ src/components/story/ButtonSet.tsx
@@ -1,80 +1,43 @@
+import { BACKGROUND_BOX, TEXT, type Cocktail } from "@/data/cocktail";
+import { cn } from "@/lib/utils";
 import * as htmlToImage from "html-to-image";
-import { twMerge } from "tailwind-merge";
-interface Props {
-  accent: "salmon" | "peach" | "pineapple" | "leaf" | "tropical" | "lavender";
-  bg:
-    | "raspberrybox"
-    | "cosmobox"
-    | "pinabox"
-    | "ginbox"
-    | "lagoonbox"
-    | "lalemonbox";
-  img: string;
-  id: string;
-}
+interface Props extends Cocktail {}
 
-const ButtonSet = ({ accent, bg, img, id }: Props) => {
-  const background = {
-    raspberrybox: "bg-raspberrybox",
-    cosmobox: "bg-cosmobox",
-    pinabox: "bg-pinabox",
-    ginbox: "bg-ginbox",
-    lagoonbox: "bg-lagoonbox",
-    lalemonbox: "bg-lalemonbox",
-  };
-
-  const acc = {
-    salmon: "text-salmon",
-    peach: "text-peach",
-    pineapple: "text-pineapple",
-    leaf: "text-leaf",
-    tropical: "text-tropical",
-    lavender: "text-lavender",
-  };
-
+const ButtonSet = ({ variant }: Props): JSX.Element => {
   const downloadImage = () => {
-    document.getElementById("buttonSet").style.visibility = "hidden";
-    htmlToImage
-      .toJpeg(document.getElementById(id), { quality: 0.96 })
-      .then(function (dataUrl) {
-        const link = document.createElement("a");
-        link.download = "result.jpeg";
-        link.href = dataUrl;
-        link.click();
-        document.getElementById("buttonSet").style.visibility = "visible";
-      });
-  };
-
-  const handleClick = () => {
-    location.href = "../";
+    const result = document.getElementById("result");
+    if (!result) return;
+    htmlToImage.toJpeg(result, { quality: 0.96 }).then(function (dataUrl) {
+      const link = document.createElement("a");
+      link.download = "result.jpeg";
+      link.href = dataUrl;
+      link.click();
+    });
   };
 
   return (
-    <div
-      id="buttonSet"
-      className="relative mx-auto my-auto mt-2 flex w-64 gap-4"
-    >
-      <button
-        className={twMerge(
+    <div className="relative mx-auto mb-16 mt-2 flex w-64 gap-4" id="buttonset">
+      <a
+        href="/"
+        className={cn(
           `flex w-full flex-wrap items-center justify-center gap-4 rounded-3xl border border-rose-200 px-2 py-1.5
-        text-center text-[1rem] font-semibold shadow-md`,
-          background[bg],
-          acc[accent]
+        text-center font-semibold shadow-md`,
+          BACKGROUND_BOX[variant],
+          TEXT[variant]
         )}
-        onClick={handleClick}
       >
         กลับสู่หน้าหลัก
-      </button>
+      </a>
       <button
-        className={twMerge(
-          `flex w-full flex-wrap items-center justify-center gap-4 rounded-3xl border border-rose-200 px-2 py-1.5
-        text-center text-[1rem] font-semibold shadow-md`,
-          background[bg],
-          acc[accent]
+        className={cn(
+          `flex w-full flex-wrap items-center justify-center gap-2 rounded-3xl border border-rose-200 px-2 py-1.5
+        text-center font-semibold shadow-md`,
+          BACKGROUND_BOX[variant],
+          TEXT[variant]
         )}
         onClick={downloadImage}
       >
-        <img className="w-[20%]" src={img} />
+        <i className="icon-[mdi--download]"></i>
         บันทึกรูป
       </button>
     </div>
--- src/components/story/Card.tsx
+++ src/components/story/Card.tsx
@@ -1,51 +1,29 @@
-import type { ReactNode } from "react";
-import { twMerge } from "tailwind-merge";
+import { useRef, type ReactNode } from "react";
 
-interface Props {
-  name: string;
-  img: string;
-  accent: "salmon" | "peach" | "pineapple" | "leaf" | "tropical" | "lavender";
-  bg:
-    | "raspberrybox"
-    | "cosmobox"
-    | "pinabox"
-    | "ginbox"
-    | "lagoonbox"
-    | "lalemonbox";
+import { cn } from "@/lib/utils";
+
+import { BACKGROUND_BOX, NAME, TEXT, type Cocktail } from "@/data/cocktail";
+
+interface Props extends Cocktail {
   children: ReactNode;
+  img: string;
 }
 
-const Card = ({ name, img, accent, bg, children }: Props) => {
-  const background = {
-    raspberrybox: "bg-raspberrybox",
-    cosmobox: "bg-cosmobox",
-    pinabox: "bg-pinabox",
-    ginbox: "bg-ginbox",
-    lagoonbox: "bg-lagoonbox",
-    lalemonbox: "bg-lalemonbox",
-  };
-
-  const acc = {
-    salmon: "text-salmon",
-    peach: "text-peach",
-    pineapple: "text-pineapple",
-    leaf: "text-leaf",
-    tropical: "text-tropical",
-    lavender: "text-lavender",
-  };
+const Card = ({ variant, children, img }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
 
   return (
     <div
-      className={twMerge(
-        `border-1 z-10 mt-1 block h-[36rem] w-[22rem] items-center justify-center
-    text-pretty rounded-2xl border-[#FFEEE8] px-2 py-2 text-center shadow-md`,
-        background[bg]
+      ref={ref}
+      className={cn(
+        "border-1 z-10 mt-1 block h-[36rem] w-[22rem] max-w-full items-center justify-center text-pretty rounded-2xl border-[#FFEEE8] px-2 py-2 text-center shadow-md",
+        BACKGROUND_BOX[variant]
       )}
     >
       <div className="mx-auto h-[35rem] rounded-2xl border border-amber-50 px-3">
-        <img src={img} className="mx-auto w-[7rem]"></img>
-        <h1 className={twMerge(`pb-2 text-2xl font-bold`, acc[accent])}>
-          {name}
+        <img src={img} className="mx-auto w-[7rem]" />
+        <h1 className={cn(`pb-2 text-2xl font-bold`, TEXT[variant])}>
+          {NAME[variant]}
         </h1>
         <div className="text-pretty pb-4 text-xs font-medium text-mutedbrown">
           {children}
