d44781015213663d0078df0f043c3accb8a9a6db
Worranittha H <86171659+mildrrnt@users.noreply.github.com>
2024-01-22 17:51:05 +0700

    feat: choice pages (#11)
    * feat: card and buttons template
    
    * style: format
    
    * fix: null score
    
    * feat: tap to continue
    
    * feat: choices and result logic
    
    * fix: handleClick props
    
    ---------
    
    Co-authored-by: shalluv <leon555thai@gmail.com>

--- src/components/story/Tap.tsx
+++ src/components/story/Tap.tsx
@@ -1,8 +1,30 @@
+import scores from "../../store/score.ts";
+const score = scores.get();
 interface Props {
-  redirect: string;
+  redirect?: string;
+  scored?: boolean;
 }
 
-const Tap = ({ redirect }: Props) => {
+const Tap = ({ redirect, scored }: Props) => {
+  if (scored) {
+    const url = {
+      0: "../cocktail/sangria",
+      1: "../cocktail/cosmopolitan",
+      2: "../cocktail/pinacolada",
+      3: "../cocktail/ginandtonic",
+      4: "../cocktail/bluelagoon",
+      5: "../cocktail/lavenderlemonade",
+    };
+    const scoreMap: { [key: string]: number } = score;
+    const fullscore = [16, 15, 16, 15, 17, 13];
+    const product: number[] = [];
+    for (let i = 0; i < fullscore.length; i++) {
+      product.push(scoreMap[i] / fullscore[i]);
+    }
+    const maxScore = Math.max(...product);
+    const maxKey = product.indexOf(maxScore);
+    redirect = url[maxKey];
+  }
   return (
     <a href={redirect} className="absolute my-auto h-full w-full opacity-50">
       <p className="absolute bottom-72 right-8 text-center font-medium">
