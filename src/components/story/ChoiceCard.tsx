9d4a5e5bc1ff30239de3e62a274f64e1bc615d48
paepaev2 <p.mwtr99@gmail.com>
2024-01-22 23:50:22 +0700

    fix: fix lint with Prettier

--- src/components/story/ChoiceCard.tsx
+++ src/components/story/ChoiceCard.tsx
@@ -9,10 +9,10 @@ interface Props {
 const ChoiceCard = ({ header, children }: Props) => {
   return (
     <div
-      className="border-1 border-mutedpurple mt-1 flex h-fit w-80 flex-wrap items-center justify-center gap-2
-        rounded-2xl border bg-white px-8 py-8 text-center  shadow-md"
+      className="border-1 mt-1 flex h-fit w-80 flex-wrap items-center justify-center gap-2 rounded-2xl
+        border border-mutedpurple bg-white px-8 py-8 text-center  shadow-md"
     >
-      <p className="text-mutedpurple mb-4 whitespace-pre-line font-['mitr'] text-lg font-bold">
+      <p className="mb-4 whitespace-pre-line font-['mitr'] text-lg font-bold text-mutedpurple">
         {header}
       </p>
       {children}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,5 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...args: ClassValue[]) {
-	return twMerge(clsx(args));
+  return twMerge(clsx(args));
 }
