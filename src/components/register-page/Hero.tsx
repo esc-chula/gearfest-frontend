7e434a751ed654b33d9c918ebd7676371bca14a4
shalluv <leon555thai@gmail.com>
2024-01-29 17:16:25 +0700

    refactor(index-page): remove unnecessary props

--- src/components/register-page/Hero.tsx
+++ src/components/register-page/Hero.tsx
@@ -1,26 +1,17 @@
 import BarImage from "@/assets/images/bar.png";
-import { cn } from "@/lib/utils";
 
-interface Props {
-  page: "register" | "start" | "redo";
-  dDay: boolean;
-}
+const Hero = () => {
+  const isDDay = new Date().getTime() > new Date("2024-02-03").getTime();
 
-const Hero = ({ page, dDay }: Props) => {
   return (
     <div className="flex w-full flex-grow flex-col items-center justify-center space-y-12">
       <div className="flex aspect-square w-full overflow-hidden rounded-lg object-cover">
         <img src={BarImage.src} alt="Bar Image" className="h-full w-full" />
       </div>
-      {dDay ? (
-        <div className={cn(page === "redo" ? "flex space-x-2" : "space-y-4")}>
-          <button
-            className={cn(
-              "flex h-fit flex-wrap items-center justify-center gap-6 rounded-lg bg-base px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button",
-              page === "redo" ? "" : "w-full"
-            )}
-          >
-            {page === "redo" ? "Redo" : "เริ่มต้นการทำแบบทดสอบ"}
+      {isDDay ? (
+        <div className="space-y-4">
+          <button className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-base px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button">
+            เริ่มต้นการทำแบบทดสอบ
           </button>
 
           <button className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-base px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button">
@@ -28,25 +19,19 @@ const Hero = ({ page, dDay }: Props) => {
           </button>
         </div>
       ) : (
-        <div className="flex justify-center">
+        <form
+          className="flex justify-center"
+          method="POST"
+          action="/api/auth/signin"
+        >
           <button
-            className={cn(
-              "flex h-fit flex-wrap items-center justify-center gap-6 rounded-lg bg-base px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button",
-              page === "redo" ? "" : "w-full"
-            )}
+            className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-base px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button"
+            type="submit"
           >
-            {page === "register" ? (
-              <>
-                <i className="icon-[devicon--google] h-7 w-7"></i>Register with
-                google
-              </>
-            ) : page === "start" ? (
-              "เริ่มต้นการทำแบบทดสอบ"
-            ) : (
-              "Redo"
-            )}
+            <i className="icon-[devicon--google] h-7 w-7"></i>Register with
+            google
           </button>
-        </div>
+        </form>
       )}
     </div>
   );
