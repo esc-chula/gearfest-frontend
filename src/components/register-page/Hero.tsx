f4582a5ffe69641127da499b6fe6c876e95a7b82
boomchanotai <boom9387@hotmail.com>
2024-01-30 00:11:54 +0700

    feat: dynamic button

--- src/components/register-page/Hero.tsx
+++ src/components/register-page/Hero.tsx
@@ -1,6 +1,10 @@
 import BarImage from "@/assets/images/bar.jpg";
 
-const Hero = (): JSX.Element => {
+interface HeroProps {
+  accessToken: string | undefined;
+}
+
+const Hero = ({ accessToken }: HeroProps): JSX.Element => {
   return (
     <div className="flex w-full flex-grow flex-col items-center justify-center space-y-12">
       <div className="flex aspect-square w-full overflow-hidden rounded-lg object-cover">
@@ -11,13 +15,30 @@ const Hero = (): JSX.Element => {
         />
       </div>
       <div className="w-full space-y-4">
-        <button className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-gray px-4 py-2.5 text-center font-sukhumvit text-xl font-bold text-primary shadow-button">
-          เริ่มต้นการทำแบบทดสอบ
-        </button>
+        {accessToken && (
+          <a href="/story/scene1">
+            <button
+              type="button"
+              className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-gray px-4 py-2.5 text-center font-sukhumvit text-xl font-bold text-primary shadow-button outline-none"
+            >
+              เริ่มต้นการทำแบบทดสอบ
+            </button>
+          </a>
+        )}
 
-        <button className="flex h-fit w-full flex-wrap items-center justify-center gap-2 rounded-lg bg-gray px-4 py-2.5 text-center font-sukhumvit text-xl font-bold text-primary shadow-button">
-          <i className="icon-[devicon--google] h-5 w-5"></i>Register with google
-        </button>
+        {!accessToken && (
+          <form action="/api/auth/signin" method="get">
+            <button
+              value="google"
+              name="provider"
+              type="submit"
+              className="flex h-fit w-full flex-wrap items-center justify-center gap-2 rounded-lg bg-gray px-4 py-2.5 text-center font-sukhumvit text-xl font-bold text-primary shadow-button outline-none"
+            >
+              <i className="icon-[devicon--google] h-5 w-5"></i>Login with
+              google
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
