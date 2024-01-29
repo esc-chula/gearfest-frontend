cf2984b1f4d8de101c117572fcc4c318351af1fa
shalluv <leon555thai@gmail.com>
2024-01-29 21:43:24 +0700

    style: prettier

--- src/components/register-page/Hero.tsx
+++ src/components/register-page/Hero.tsx
@@ -14,11 +14,11 @@ const Hero = () => {
       </div>
       {isDDay ? (
         <div className="space-y-4">
-          <button className="bg-gray flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button">
+          <button className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-gray px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button">
             เริ่มต้นการทำแบบทดสอบ
           </button>
 
-          <button className="bg-gray flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button">
+          <button className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-gray px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button">
             ลงทะเบียนเข้างาน
           </button>
         </div>
@@ -29,7 +29,7 @@ const Hero = () => {
           action="/api/auth/signin"
         >
           <button
-            className="bg-gray flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button"
+            className="flex h-fit w-full flex-wrap items-center justify-center gap-6 rounded-lg bg-gray px-4 py-2.5 text-center text-xl font-semibold text-primary shadow-button"
             type="submit"
           >
             <i className="icon-[devicon--google] h-5 w-5"></i>Register with
