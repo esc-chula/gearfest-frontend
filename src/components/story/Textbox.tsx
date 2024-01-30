4134e61e78b118c46b233bd42d2df92a2042ca8b
boomchanotai <boom9387@hotmail.com>
2024-01-30 19:56:48 +0700

    feat: missing page

--- src/components/story/Textbox.tsx
+++ src/components/story/Textbox.tsx
@@ -1,6 +1,21 @@
-const Textbox = (): JSX.Element => {
+import { useCallback } from "react";
+import { useCookies } from "react-cookie";
+
+const Textbox = ({ id }: { id?: string }): JSX.Element => {
+  const setCookie = useCookies([id || ""])[1];
+
+  const save = useCallback((text: string) => {
+    if (id)
+      setCookie(id, text, {
+        path: "/",
+      });
+  }, []);
+
   return (
     <textarea
+      onChange={(e) => {
+        save(e.target.value);
+      }}
       placeholder="เขียนคำตอบของคุณ...."
       className="h-48 w-full rounded-lg bg-mutedpurple p-2 text-sm text-white placeholder:text-white/80 focus:outline-none"
     />
