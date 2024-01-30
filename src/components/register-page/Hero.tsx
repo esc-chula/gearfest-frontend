a81bc8fb3e8f5d7e0fe6362e663cb206ca4d2942
boomchanotai <boom9387@hotmail.com>
2024-01-31 00:31:45 +0700

    refactor: convert to webp

--- src/components/register-page/Hero.tsx
+++ src/components/register-page/Hero.tsx
@@ -1,5 +1,5 @@
-import BarImage from "@/assets/images/bar.jpg";
-import PageImage from "@/assets/images/Scene14.gif";
+import BarImage from "@/assets/images/bar.webp";
+import PageImage from "@/assets/images/Scene14.webp";
 import parser from "@/utils/parser";
 import { useCallback, useMemo } from "react";
 import { useCookies } from "react-cookie";
