e2531a61971b23421bb798eb5ab4b154bdc5bc5c
shalluv <leon555thai@gmail.com>
2024-01-30 18:02:57 +0700

    fix(edge-functions): removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,5 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
+  adapter: netlify(),
 });
