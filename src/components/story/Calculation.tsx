6e3d55ff754e11941bf6e847bc9f93d710c81385
boomchanotai <boom9387@hotmail.com>
2024-01-30 09:53:19 +0700

    feat: set cookie for cocktail

--- src/components/story/Calculation.tsx
+++ src/components/story/Calculation.tsx
@@ -1,19 +1,20 @@
+import scores from "@/store/score";
 import { useStore } from "@nanostores/react";
 import { useEffect } from "react";
-
-import scores from "@/store/score";
+import { useCookies } from "react-cookie";
 
 const Calculation = () => {
   const $scores = useStore(scores);
+  const setCookie = useCookies(["cocktail"])[1];
 
   useEffect(() => {
     const url: Record<number, string> = {
-      0: "/cocktail/sangria",
-      1: "/cocktail/cosmopolitan",
-      2: "/cocktail/pinacolada",
-      3: "/cocktail/ginandtonic",
-      4: "/cocktail/bluelagoon",
-      5: "/cocktail/lavenderlemonade",
+      0: "sangria",
+      1: "cosmopolitan",
+      2: "pinacolada",
+      3: "ginandtonic",
+      4: "bluelagoon",
+      5: "lavenderlemonade",
     };
     const scoreMap: { [key: string]: number } = $scores;
     const fullscore = [40, 52, 45.5, 49, 49.5, 42.5];
@@ -23,7 +24,8 @@ const Calculation = () => {
     }
     const maxScore = Math.max(...product);
     const maxKey = product.indexOf(maxScore);
-    if (window) window.location.href = url[maxKey];
+    setCookie("cocktail", url[maxKey].toString());
+    if (window) window.location.href = "/cocktail/" + url[maxKey];
   }, []);
 };
 
