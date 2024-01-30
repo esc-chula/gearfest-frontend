28be38635aacdd5c5deefdaf248bee24e5e29c20
boomchanotai <boom9387@hotmail.com>
2024-01-30 09:40:41 +0700

    feat: protected route without validation

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,15 @@
 import { defineMiddleware } from "astro:middleware";
 
-export const onRequest = defineMiddleware((context, next) => {
-  // TODO: Implement auth routes logic
-  console.log("Middleware works!");
+export const onRequest = defineMiddleware(
+  ({ cookies, request, redirect }, next) => {
+    const cookie = cookies.get("sb-access-token")?.value;
 
-  return next();
-});
+    if (
+      (request.url.includes("/story") || request.url.includes("/cocktail")) &&
+      !cookie
+    ) {
+      return redirect("/");
+    }
+    return next();
+  }
+);
